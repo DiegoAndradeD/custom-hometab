45171d0624385f92dc1de7a2525c654904412cb6
DiegoAndradeD <diegoadeiro313@gmail.com>
2025-10-31 18:20:15 -0300

    fix lint

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,7 +1,6 @@
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+import { cn } from "../../lib/utils";
 
 function Slider({
   className,
@@ -16,10 +15,10 @@ function Slider({
       Array.isArray(value)
         ? value
         : Array.isArray(defaultValue)
-          ? defaultValue
-          : [min, max],
+        ? defaultValue
+        : [min, max],
     [value, defaultValue, min, max]
-  )
+  );
 
   return (
     <SliderPrimitive.Root
@@ -55,7 +54,7 @@ function Slider({
         />
       ))}
     </SliderPrimitive.Root>
-  )
+  );
 }
 
-export { Slider }
+export { Slider };
